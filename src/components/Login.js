97b3f4723dc88b41af6c99ab921ccbf81d6cedb1
Marty Siksnis <siksnis@Martys-MacBook-Pro.local>
2020-10-13 13:47:11 +0200

    Started the app with react-create-app, did cleanup, started working on Login component.

---
+++ src/components/Login.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Login.css";
+
+function Login() {
+  return (
+    <div className="login">
+      <img
+        className="login__logo"
+        src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
+        alt="Spotify Logo"
+      />
+      <a>LOGIN WITH SPOTIFY</a>
+      {/* Spotify Logo */}
+      {/* Login with Spotify button */}
+    </div>
+  );
+}
+
+export default Login;
