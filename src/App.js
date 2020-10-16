c8d52f1934ed90704184516419146b2eaaf48701
Marty Siksnis <siksnis@Martys-MacBook-Pro.local>
2020-10-16 10:49:18 +0200

    Finished for now with a Login component which gets back the token and now with SpotifyWebApi package I have access to spotify.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Login from "./components/Login";
+import Player from "./components/player/Player";
+import { getTokenFromUrl } from "./spotify";
+import SpotifyWebApi from "spotify-web-api-js";
+
+const spotify = new SpotifyWebApi();
 
 function App() {
-  return (
-    <div className="app">
-      <Login />
-    </div>
-  );
+  const [token, setToken] = useState(null);
+
+  // useEffect runs code based on given condition
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token);
+
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then((user) => {
+        console.log(user);
+      });
+    }
+
+    console.log("this is token", token);
+  }, []);
+
+  return <div className="app">{token ? <Player /> : <Login />}</div>;
 }
 
 export default App;
